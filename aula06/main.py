da3698c0536c44e3dcdb9ccef17ff4b4c1fb49d4
JeanCarlosHP <jeanchpaula@gmail.com>
2021-05-29 21:02:49 -0300

    Encapsulamento

---
+++ aula06/main.py
@@ -0,0 +1,37 @@
+"""
+public, protected, private
+
+_ protected
+__ private
+"""
+
+
+class BaseDeDados:
+    def __init__(self):
+        self.__dados = {}
+
+    @property
+    def dados(self):
+        return self.__dados
+
+    def inserir_cliente(self, id, nome):
+        if 'clientes' not in self.__dados:
+            self.__dados['clientes'] = {id: nome}
+            return
+
+        self.__dados['clientes'].update({id: nome})
+
+    def lista_clientes(self):
+        for id, nome in self.__dados['clientes'].items():
+            print(id, nome)
+
+    def apaga_cliente(self, id):
+        del self.__dados['clientes'][id]
+
+
+bd = BaseDeDados()
+bd.inserir_cliente(1, 'Jean')
+bd.inserir_cliente(2, 'Gustavo')
+bd.inserir_cliente(3, 'William')
+bd.dados = 'Outra coisa'
+print(bd.dados)
